d1323bb148326ca694d44634dbd14c7d74b31873
Adam Wolff <adamkwolff@gmail.com>
2020-10-04 08:33:10 -0500

    Added in MY info

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /**
-* Template Name: iPortfolio - v1.4.0
+* Template Name: iPortfolio - v1.4.1
 * Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
