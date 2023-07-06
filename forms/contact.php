67664511905422dcee386fd075a6d90ba6e4248d
Adam Wolff <adamwolff@Adams-MBP.home>
2023-07-06 10:48:12 -0500

    Trying to get the correct smtp credentials

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,13 @@ $contact->from_email = $_POST['email'];
 $contact->subject = $_POST['subject'];
 
 // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-/*
 $contact->smtp = array(
-'host' => 'example.com',
-'username' => 'example',
-'password' => 'pass',
-'port' => '587'
+  'host' => 'smtp.office365.com',
+  'username' => 'adam@adamkwolff.com',
+  'password' => 'Fflow0667!!!',
+  'port' => '587',
+  'encryption' => 'tls'
 );
-*/
 
 $contact->add_message( $_POST['name'], 'From');
 $contact->add_message( $_POST['email'], 'Email');
